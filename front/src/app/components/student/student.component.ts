2863d171837b79ee35fabfb4fa64ff279ff5a417
Dylan Hernandez <105333109+dylanhdz@users.noreply.github.com>
2024-07-21 21:11:08 -0500

    Update and Delete Frontend

--- front/src/app/components/student/student.component.ts
+++ front/src/app/components/student/student.component.ts
@@ -18,6 +18,9 @@ export class StudentComponent implements OnInit{
   email: string = '';
   password: string = '';
   courses: Course[] = [];
+  selectedCourseId: number = 0;
+  selectedCourseName: string = '';
+  selectedStudentId: number | null = null;
 
   constructor(private studentService: StudentService, private courseService: CourseService) {}
 
@@ -29,12 +32,14 @@ export class StudentComponent implements OnInit{
   loadStudents(): void {
     this.studentService.getStudents().subscribe((data: Student[]) => {
       this.students = data;
+      this.updateSelectedStudent(this.students[0].id?.toString() || '');
     });
   }
 
   loadCourses(): void {
     this.courseService.getCourses().subscribe((data: Course[]) => {
       this.courses = data;
+      this.updateSelectedCourse(this.courses[0].id + ' - ' + this.courses[0].nombre);
     });
   }
 
@@ -57,18 +62,78 @@ export class StudentComponent implements OnInit{
     });
   }
 
-  enrollStudentInCourse(student: Student, event: Event): void {
-    const selectElement = event.target as HTMLSelectElement;
-    const courseId = parseInt(selectElement.value.split('-')[0]);
-    this.courseService.enrollStudentInCourse(student, courseId).subscribe({
+  updateSelectedCourse(value: string): void {
+    const parts = value.split(' - ');
+    this.selectedCourseId = parseInt(parts[0]);
+    this.selectedCourseName = parts[1];
+  }
+
+  updateSelectedStudent(value: string): void {
+    this.selectedStudentId = parseInt(value);
+    this.loadSelectedStudent();
+  }
+  
+  enrollStudentInCourse(student: Student): void {
+    this.courseService.enrollStudentInCourse(student, this.selectedCourseId).subscribe({
       next: (response) => {
         // Handle successful enrollment
-        alert('Se ha agregado correctamente al estudiante ' + student.nombre + ' al curso "' + selectElement.value.split('-')[1] + '"');
+        alert('Se ha agregado correctamente al estudiante ' + student.nombre + ' al curso "' + this.selectedCourseName + '"');
       },
       error: (error) => {
         // Handle error
         alert("El usuario ya está inscrito en un curso.");
       }
     });
+  }
+
+  deleteStudent(student: Student): void {
+    if (student.id !== undefined) {
+      this.studentService.deleteStudent(student.id).subscribe({
+        next: (response) => {
+          alert('Se ha eliminado el estudiante ' + student.nombre);
+          this.loadStudents();
+        },
+        error: (error) => {
+          alert('Error al eliminar el estudiante');
+        }
+      });
+    } else {
+      alert('Error: student.id is undefined');
+    }
+  }
+  trackByStudentId(index: number, student: Student): number | undefined {
+    return student.id;
+  }
+
+  loadSelectedStudent(): void {
+    if (this.selectedStudentId !== null) {
+      this.studentService.getStudent(this.selectedStudentId).subscribe((student: Student) => {
+        this.nombreUsuario = student.nombre;
+        this.email = student.email;
+        this.password = ''; // Passwords are usually not retrieved for editing
+      });
+    }
+  }
+
+  updateStudent(): void {
+    if (this.selectedStudentId) {
+      const student: Student = {
+        id: this.selectedStudentId,
+        nombre: this.nombreUsuario,
+        email: this.email,
+        password: this.password
+      };
+      this.studentService.updateStudent(student).subscribe({
+        next: (response) => {
+          alert('Se ha editado el estudiante ' + student.nombre);
+          this.loadStudents();
+        },
+        error: (error) => {
+          alert('Error al editar el estudiante');
+        }
+      });
+    } else {
+      alert('Seleccione un estudiante para editar');
+    }
   }
 }
